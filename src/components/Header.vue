d31304f1257bcc4a32c39b8338ae15fdb68dae41
aotuotage <826931037@qq.com>
2020-06-24 14:26:30 +0800

    4545544

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="ac_header">
+    刘爽最美
+  </div>
+</template>
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.ac_header{
+  width:100%;
+  height: 50px;
+  font-size: 18px;
+  color:#fff;
+  line-height:50px;
+  background-color: #d0b482;
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="ac_list">
+    <Header></Header>
+    <ul>
+      <li v-bind:key="index" v-for="(item,index) in list"><a href="">{{item}}</a></li>
+    </ul>
+  </div>
+</template>
+<script>
+import Header from '@/components/Header'
+import list from '../assets/data/list.js' // 引入
+export default {
+  name: 'List',
+  components:{
+    Header
+  },
+  data () {
+    return {
+      list: []
+    }
+  },
+  created () {
+    this.list = list.list
+    console.log(list)
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.ac_list ul{
+  list-style: none;
+  padding: 0;
+  margin: 40px 0 0 0;
+}
+.ac_list ul li{
+  width:100%;
+  height: 40px;
+  border-bottom: 1px solid #999;
+}
+.ac_list ul li a{
+  line-height:40px;
+  color: #333;
+  font-size: 18px;
+  text-decoration: none;
+}
+</style>
