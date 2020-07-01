611671b5abb688a89a519c279be1d5a86081742f
aotuotage <826931037@qq.com>
2020-07-01 16:26:04 +0800

    454554

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -19,7 +19,15 @@ export default {
   },
   methods:{
     gotop(){
-      document.documentElement.scrollTop = 0
+       var timer = setInterval(function(){
+        let osTop = document.documentElement.scrollTop || document.body.scrollTop;
+        let ispeed = Math.floor(-osTop / 5); 
+        document.documentElement.scrollTop = document.body.scrollTop = osTop + ispeed;
+        this.isTop = true;
+        if(osTop === 0){
+          clearInterval(timer);
+        }
+      },30)
     }
   }
 }
